a2757803ff2456a68dd3f8821a0889065350243e
Rustem Nasyrov <nasyroff.rus@gmail.com>
2026-04-13 23:28:06 +0400

    feat: add LeafNode subclass with HTML rendering
    - Implement LeafNode for nodes without children
    - Add to_html, equality, and repr methods
    - Include unit tests for LeafNode functionality

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,3 +23,23 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, children=None, props=props)
+
+    def to_html(self):
+        if self.value is None or self.value == "":
+            raise ValueError()
+        if self.tag is None or self.tag == "":
+            return self.value
+        props = " " + self.props_to_html() if self.props is not None or self.props is not None and len(
+            self.props) > 0 else ""
+        return f"<{self.tag}{props}>{self.value}</{self.tag}>"
+
+    def __eq__(self, other):
+        return self.tag == other.tag and self.value == other.value and self.props == other.props
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -20,3 +20,25 @@ class TestHTMLNode(unittest.TestCase):
         })
         prop_str = 'href="https://www.google.com" target="_blank"'
         self.assertEqual(node.props_to_html(), prop_str)
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_eq(self):
+        leaf = LeafNode("p", "Lorem")
+        leaf2 = LeafNode("p", "Lorem")
+        self.assertEqual(leaf, leaf2)
+
+    def test_not_eq_text(self):
+        leaf = LeafNode(tag="span", value="Foo")
+        leaf2 = LeafNode(tag="span", value="Bar")
+        self.assertNotEqual(leaf, leaf2)
+
+    def test_to_html(self):
+        leaf_html = LeafNode(tag="p", value="Hello, World!").to_html()
+        html_str = '<p>Hello, World!</p>'
+        self.assertEqual(leaf_html, html_str)
+
+    def test_no_tag(self):
+        value = "Text"
+        leaf_html = LeafNode(tag=None, value=value).to_html()
+        self.assertEqual(leaf_html, value)
