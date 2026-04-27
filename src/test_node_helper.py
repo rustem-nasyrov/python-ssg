45e102763e88b9ab3876ac18ff02c6e68080c3af
Rustem Nasyrov <nasyroff.rus@gmail.com>
2026-04-27 23:57:19 +0400

    test: add tests for splitting images and links
    - Add tests for extracting markdown images and links
    - Cover multiple occurrences, trailing text, and non-text node preservation
    - Ensure link splitter doesn't treat images as links

--- src/test_node_helper.py
+++ src/test_node_helper.py
@@ -1,5 +1,5 @@
 import unittest
-from node_helper import split_nodes_delimiter, extract_markdown_images
+from node_helper import split_nodes_delimiter, extract_markdown_images, split_nodes_image, split_nodes_link
 from src.node_helper import extract_markdown_links
 from textnode import TextType, TextNode
 
@@ -43,6 +43,69 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             TextNode("in the end", TextType.ITALIC),
         ]
         self.assertEqual(outcome, target)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode(
+                    "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_images_ignores_non_text_nodes(self):
+        node = TextNode("![image](https://example.com/image.png)", TextType.BOLD)
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual([node], new_nodes)
+
+    def test_split_links_with_trailing_text(self):
+        node = TextNode(
+            "[home](https://example.com) is the homepage",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("home", TextType.LINK, "https://example.com"),
+                TextNode(" is the homepage", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links_does_not_parse_images(self):
+        node = TextNode(
+            "This is an image ![alt](https://example.com/image.png), not a link",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual([node], new_nodes)
 
 
 class TestExtract(unittest.TestCase):
