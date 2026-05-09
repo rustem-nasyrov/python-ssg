8396a21712f1d9f14c4852bc7a3eb925f496eac1
Rustem Nasyrov <nasyroff.rus@gmail.com>
2026-05-09 21:16:24 +0400

    feat: serve public and generate index
    - Call generate_page to render content/index.md to public/index.html
    - Start simple HTTP server serving public on port 8888

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
+from src.generator import generate_page
 from src.static_to_public import copy_static_to_public
 
 
 def main() -> None:
     copy_static_to_public()
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 if __name__ == "__main__":
